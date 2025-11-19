3cffdbc47428f8adadd9feb2e562bb48eb503091
Beranger Scherpereel <beranger.scherpereel@gmail.com>
2025-11-19 10:44:27 +0100

    first commit

---
+++ src/main/java/tpd/dmx_control/App.java
@@ -0,0 +1,13 @@
+package tpd.dmx_control;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
